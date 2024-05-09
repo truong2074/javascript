1b682cfbac91750589c38fb486e521e909285641
NguyenQuocTruong_2122110532 <153279706+truong2074@users.noreply.github.com>
2024-05-09 18:39:37 +0700

    Add files via upload

---
+++ Buoi1_NguyenQuocTruong_2122110532/bai1.js
@@ -0,0 +1,6 @@
+// let masv1=2122110532 
+// let hoten1="nguyen quoc truong"
+// console.log(masv);
+// console.log(hoten);
+
+// document.getElementById("show").innerHTML=`Ma sinh vien:${masv1} ho ten:${hoten1}`;
---
+++ Buoi1_NguyenQuocTruong_2122110532/bai2.js
@@ -0,0 +1,14 @@
+document.getElementById("show").innerHTML=`
+<tr>
+    <td>STT</td>
+    <td>MASV</td>
+    <td>HOTEN</td>
+    <td>LOP</td>
+</tr>
+<tr>
+    <td>STT</td>
+    <td>MASV</td>
+    <td>HOTEN</td>
+    <td>LOP</td>
+</tr>
+`;
